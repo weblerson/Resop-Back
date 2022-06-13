05004f793248bc9e5fef8bb943f779f7ff8f288d
weblerson <zonejr4@gmail.com>
2022-06-12 21:45:58 -0400

    tokens and verifiers

--- API.py
+++ API.py
@@ -5,7 +5,7 @@ from typing import Optional
 from fastapi.middleware.cors import CORSMiddleware
 from models import Users, Tokens
 import bcrypt
-from contrib import Verify
+from contrib import Verify, Token
 
 class User(BaseModel):
     ID: Optional[int]
@@ -45,4 +45,31 @@ def register(user: User):
 
 @app.post("/login")
 def login(user: User):
-    pass
+    user_exists = Verify.user_credentials(f"{user.email}@lerson.com", user.senha)
+    user_have_token = Token.verify_user_token(email = f"{user.email}@lerson.com")
+
+    if user_exists["success"]:
+        if user_have_token["success"]:
+            token_is_not_expired = Token.verify_token(token = user_have_token["response"])
+
+            if token_is_not_expired["success"]:
+                #final
+                return {"success": True, "response": token_is_not_expired["response"]}
+
+            else:
+                new_token = Token.update(email = f"{user.email}@lerson.com")
+                if new_token["success"]:
+                    #final
+                    return {"success": True, "response": new_token["response"]}
+
+                return {"success": False, "response": new_token["response"]}
+
+        else:
+            created_token = Token.create(email = f"{user.email}@lerson.com")
+            if created_token["success"]:
+                #final
+                return {"success": True, "response": created_token["response"]}
+
+            return {"success": False, "response": created_token["response"]}
+
+    return {"success": False, "response": user_exists["response"]}
--- contrib.py
+++ contrib.py
@@ -1,6 +1,8 @@
 from config import *
 from models import Users, Tokens
 from data import Payload
+import bcrypt
+import datetime
 import jwt
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives import serialization
@@ -26,6 +28,37 @@ class Verify:
         except:
             return {"success": False, "response": "Erro ao consultar o banco."}
 
+    @staticmethod
+    def user_credentials(email: str, password: str):
+        if session.query(session.query(Users).filter(Users.email == email).exists()).one()[0]:
+            user = session.query(Users).filter(Users.email == email).one()
+
+            if bcrypt.checkpw(password.encode(), user.senha.encode()):
+                return {"success": True, "response": ""}
+
+            return {"success": False, "response": "A senha passada está incorreta."}
+
+        return {"success": False, "response": "O endereço de email passado não existe."}
+
+    @staticmethod
+    def user_token(email: str, token: str):
+        if session.query(session.query(Tokens).filter(Tokens.token == token).exists()).one()[0]:
+            token = session.query(Tokens).filter(Tokens.token == token).one()
+            user = session.query(Users).filter(Users.email == token.email_usuario).one()
+
+            try:
+                decoded = jwt.decode(token, key = user.public_key, algorithms = ["RS256"])
+
+                if decoded["email"] == email:
+                    #Autoriza ação
+                    return {"success": True, "response": ""}
+
+                else:
+                    #Nega ação
+                    return {"success": False, "response": ""}
+
+            except:
+                return {"success": False, "response": ""}
 
 class Keys:
     @staticmethod
@@ -73,10 +106,50 @@ class Token:
                 return {"success": False, "response": "Ocorreu um erro ao se comunicar com o banco"}
 
         else:
-            return payload_data
+            return {"success": False, "response": payload_data["response"]}
 
     @staticmethod
-    def verify(token: str):
+    def update(email: str):
+        try:
+            private_key, public_key = Keys.generate_keys()
+            payload_data = Payload.toJSON(email = email)
+
+            if payload_data["success"]:
+                token = jwt.encode(payload = payload_data, key = private_key, algorithm = "RS256")
+
+                session.query(Tokens).filter(Tokens.email_usuario == email).update({
+                    "token": token,
+                    "data_criacao": datetime.datetime.now(),
+                    "public_key": public_key
+                })
+
+                session.commit()
+
+                return {"success": True, "response": token}
+
+        except:
+            return {"success": False, "response": "Ocorreu um erro ao consultar o banco."}
+
+    @staticmethod
+    def verify_token(token: str):
         if session.query(session.query(Tokens).filter(Tokens.token == token).exists()).one()[0]:
-            pass
+            user_data = session.query(Tokens).filter(Tokens.token == token).one()
+            date_now = datetime.datetime.now()
+
+            interval = date_now.date() - user_data.data_criacao.date()
+
+            #interval.days
+            if interval.days < 15:
+                return {"success": True, "response": token}
+
+            return {"success": False, "response": ""}
+
+    @staticmethod
+    def verify_user_token(email: str):
+        if session.query(session.query(Tokens).filter(Tokens.email_usuario == email).exists()).one()[0]:
+            user = session.query(Tokens).filter(Tokens.email_usuario == email).one()
+
+            return {"success": True, "response": user.token}
+
+        return {"success": False, "response": ""}
         
